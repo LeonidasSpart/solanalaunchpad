8377fd906ba647f45fb4e3ed9749fbf079a9a459
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-06-28 23:47:45 +0200

    Refactor Home component layout and add new sections

--- solana-token-launcher-main/src/app/page.tsx
+++ solana-token-launcher-main/src/app/page.tsx
@@ -4,16 +4,24 @@ import FeaturedTokens from '@/components/Home/featured-tokens';
 import FeaturesSection from '@/components/Home/features-section';
 import CtaSection from '@/components/Home/cta-section';
 import TokensSection from '@/components/Home/tokens-section';
+import TokenCounter from '@/components/Home/token-counter';
+import FAQ from '@/components/Home/faq';
+import WhyUs from '@/components/Home/why-us';
+import DemoSection from '@/components/Home/demo-section';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
+      <TokenCounter />
       <HowItWorks />
-      <FeaturedTokens />
+      <DemoSection />
+      <WhyUs />
       <FeaturesSection />
-      <CtaSection />
+      <FeaturedTokens />
       <TokensSection />
+      <FAQ />
+      <CtaSection />
     </div>
   );
 }

3117b69e2409f8080453817b6b33d974bbe7201b
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-06-29 00:48:40 +0200

    Clean up unused TokensSection in Home component
    Removed unused TokensSection import and component.

--- solana-token-launcher-main/src/app/page.tsx
+++ solana-token-launcher-main/src/app/page.tsx
@@ -3,8 +3,7 @@ import HowItWorks from '@/components/Home/how-it-works';
 import FeaturedTokens from '@/components/Home/featured-tokens';
 import FeaturesSection from '@/components/Home/features-section';
 import CtaSection from '@/components/Home/cta-section';
-import TokensSection from '@/components/Home/tokens-section';
-// IMPORTANT: These must be imported
+// Remove this: import TokensSection from '@/components/Home/tokens-section';
 import TokenCounter from '@/components/Home/token-counter';
 import FAQ from '@/components/Home/faq';
 import WhyUs from '@/components/Home/why-us';
@@ -14,14 +13,14 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
-      <TokenCounter />        {/* Is this here? */}
+      <TokenCounter />
       <HowItWorks />
-      <DemoSection />         {/* Is this here? */}
-      <WhyUs />               {/* Is this here? */}
+      <DemoSection />
+      <WhyUs />
       <FeaturesSection />
       <FeaturedTokens />
-      <TokensSection />
-      <FAQ />                 {/* Is this here? */}
+      {/* Remove this: <TokensSection /> */}
+      <FAQ />
       <CtaSection />
     </div>
   );

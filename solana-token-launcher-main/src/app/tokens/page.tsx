ebe700c5162a8e7b769979fc1f7898cdac069cd1
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-06-30 18:13:04 +0200

    Refactor TokensPage component layout

--- solana-token-launcher-main/src/app/tokens/page.tsx
+++ solana-token-launcher-main/src/app/tokens/page.tsx
@@ -2,12 +2,13 @@
 
 import React from 'react';
 import DisplayTokens from '@/components/Collection/DisplayTokens';
-const TokensPage=()=>{
-  return (
-    <div>
-      <DisplayTokens/>
-    </div>
-  )
-}
-export default TokensPage;
 
+const TokensPage = () => {
+  return (
+    <div className="min-h-screen bg-black">
+      <DisplayTokens />
+    </div>
+  );
+};
+
+export default TokensPage;
